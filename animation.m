196a8b125e9aa04264cbc7f861c5c008d7e77618
cos-sta <126566950+cos-sta@users.noreply.github.com>
2023-10-30 14:47:04 +1100

    New Animation
    This includes ball movement

--- animation.m
+++ animation.m
@@ -11,18 +11,31 @@ kukarobot = Kuka16;
 hold on
 axis auto
 
-
 %% ------------ANIMATION FOR PROMO VIDEO-------------- %%
+%% Set up the Ball matrix
 
-% % Create a ball object
-% ballLocation = [-1, 1, 1]; % Initial location of the ball
-% ball = Ball(ballLocation);
+Ball_Matrix = [
+    -3.2, 4, 0.75];
 
-% Move LinearUR3 to the desired end effector position
-linear_ur3_target = transl(2.65, 3.15, 0.5);
-q_linear_ur3 = LinearUR3.model.ikcon(linear_ur3_target);
+% Plot the ball
+Ball = RobotCows(1);
+
+for id = 1:size(Ball_Matrix, 1)
+    Ball.cowModel{id}.base = SE3(Ball_Matrix(id,:)).T;
+    Ball.cowModel{id}.animate(0);
+end
+
+%% Move LinearUR3 to the desired end effector position
+% Define initial orientation
+roll_L = 0; pitch_L = 0; yaw_L = 0;
+initial_orientation_Linear = rpy2tr(roll_L, pitch_L, yaw_L);
+
+% linear_ur3_target = transl(2.65, 3.15, 1.5)*initial_orientation_Linear;
+q_linear_ur3 = [0, -90, -90, 0, -90, -90, 0, 0]; % LinearUR3.model.ikcon(linear_ur3_target);
 LinearUR3.model.animate(q_linear_ur3);
 drawnow;
+
+input('Press enter to continue');
 
 % Current pose of the robot
 q = zeros(1, kukarobot.model.n);
@@ -31,15 +44,25 @@ q = zeros(1, kukarobot.model.n);
 roll = 0; pitch = -pi/2; yaw = 0;
 initial_orientation = rpy2tr(roll, pitch, yaw);
 
-% Step 1: Move to the Desired Location [-3, 3.5, 0.5]
-desired_xyz = [-1, 1, 1];
+%% Step 1: Move to the Desired Location [-1, 1, 1] Where the ball is located
+desired_xyz = [-3.2,4,1];
 desired_pose = transl(desired_xyz) * initial_orientation;
 
 % Cartesian trajectory to the desired location
 T1 = SE3(kukarobot.model.fkine(q));
 T2 = SE3(desired_pose);
 q_traj_initial = generate_trajectory(kukarobot, T1, T2, 50, q);
+
+% Execute the trajectory to the ball without the ball
 execute_trajectory(kukarobot, q_traj_initial);
+
+% Calculate the forward kinematics for Kuka at the end of the initial trajectory
+T_kuka = kukarobot.model.fkine(q_traj_initial(end, :));
+
+%% Attach the ball to Kuka's end effector for the initial position
+update_ball_position(Ball, T_kuka);
+
+% input('Press enter to continue');
 
 % Compute the forward kinematics for Kuka at the end of the initial trajectory
 T_kuka = kukarobot.model.fkine(q_traj_initial(end, :));
@@ -52,8 +75,6 @@ direction_vector = (transl(T_linear_ur3) - transl(T_kuka)) / norm(transl(T_linea
 
 % Calculate the new orientation for Kuka's end effector to face LinearUR3
 R = look_at(direction_vector, [0, 0, 1]);
-
-input('Press enter to continue');
 
 % Compute the forward kinematics for LinearUR3 and Kuka
 T_linear_ur3 = LinearUR3.model.fkine(q_linear_ur3);
@@ -68,9 +89,11 @@ perpendicular_point = [T_linear_ur3_translation(1), T_linear_ur3_translation(2),
 parallel_pose = rt2tr(R, perpendicular_point);
 T3 = SE3(parallel_pose);
 q_traj_perpendicular = generate_trajectory(kukarobot, T2, T3, 30, q_traj_initial(end, :));
-execute_trajectory(kukarobot, q_traj_perpendicular);
 
-input('Press enter to continue');
+%% Execute the trajectory from the ball to being perepndicular to the end effectory of the LinearUR3
+execute_trajectory_with_ball(kukarobot, q_traj_perpendicular, Ball, true)
+
+% input('Press enter to continue');
 
 %% Step 3: Throwing Motion
 % Parameters
@@ -91,22 +114,50 @@ for i = 1:num_steps_windup
     q_traj_windup(i, :) = (1 - i/num_steps_windup) * q_current + (i/num_steps_windup) * q_wind_up;
 end
 
-% Execute the trajectory
-execute_trajectory(kukarobot, q_traj_windup);
+execute_trajectory_with_ball(kukarobot, q_traj_windup, Ball, true);
+
+% input('Press enter to continue');
 
 % After the wind-up motion, update T_kuka for the next calculations
 T_kuka = kukarobot.model.fkine(q_traj_windup(end, :));
 
-input('Press enter to continue');
-
 % Generate a faster throw trajectory by using fewer steps
 q_traj_throw = flipud(q_traj_windup(1:num_steps_throw, :));
 
-% Execute the reverse (throw) trajectory
-execute_trajectory(kukarobot, q_traj_throw);
+% Execute the throw trajectory with the ball attached, then detached
+detach_step = 15;  % Detach ball after this step
+execute_trajectory_with_ball(kukarobot, q_traj_throw(1:detach_step, :), Ball, true);
+execute_trajectory_with_ball(kukarobot, q_traj_throw(detach_step+1:end, :), Ball, false);
 
 
-% Function to execute a trajectory
+%% Step 4: Move Ball from Kuka's end effector to LinearUR3's end effector
+%% Animate Ball from Kuka's End Effector to LinearUR3's End Effector
+
+% Ensure T_kuka is the final transformation of Kuka after all movements
+T_kuka_final = kukarobot.model.fkine(q_traj_throw(end, :));
+
+% Final position of the ball at Kuka's end effector (already calculated)
+start_position = transl(T_kuka_final);
+
+% Position of LinearUR3's end effector
+end_position = transl(T_linear_ur3);
+
+% Number of steps in the animation
+num_steps = 100;
+
+% Create a linear trajectory from start_position to end_position
+for i = 1:num_steps
+    % Linear interpolation
+    interp_position = start_position + (end_position - start_position) * (i / num_steps);
+
+    % Update the position of the ball
+    update_ball_position(Ball, transl(interp_position));
+    
+    pause(0.02); % Pause for animation effect
+end
+
+
+%% -------------- Function to execute a trajectory ------------------%%
 function execute_trajectory(robot, q_traj)
     for i = 1:size(q_traj, 1)
         robot.model.animate(q_traj(i, :));
@@ -124,6 +175,20 @@ function q_traj = generate_trajectory(robot, start_T, end_T, steps, initial_q)
     end
 end
 
+%% -------------- Function to execute a trajectory ------------------%%
+function execute_trajectory_with_ball(robot, q_traj, Ball, update_ball)
+    for i = 1:size(q_traj, 1)
+        robot.model.animate(q_traj(i, :));
+        if update_ball
+            T_current = robot.model.fkine(q_traj(i, :));
+            update_ball_position(Ball, T_current);
+        end
+        drawnow;
+        pause(0.02);
+    end
+end
+
+
 function R = look_at(direction, up)
     z = direction(:) / norm(direction);
     y = up(:) - z * dot(up(:), z);
@@ -131,6 +196,16 @@ function R = look_at(direction, up)
     x = cross(y, z);
     R = [x y z];
 end
+
+%% Function to update ball position based on Kuka's end effector
+function update_ball_position(Ball, T)
+    for id = 1:size(Ball.cowModel, 2)
+        Ball.cowModel{id}.base = T;
+        Ball.cowModel{id}.animate(0);
+    end
+    drawnow;
+end
+
 
 
 
